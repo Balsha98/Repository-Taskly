0bbb92d583cee91604908bd6ddaa075860a4f387
Balša Bazović <balsa.bazovic@gmail.com>
2025-10-22 02:00:37 -0400

    Updated App script.

--- taskly/src/assets/js/components/App.js
+++ taskly/src/assets/js/components/App.js
@@ -36,7 +36,7 @@ export default function App() {
                     onUpdateTasks={setTasks}
                 />
             ) : (
-                <TasksOverview />
+                <TasksOverview tasks={tasks} />
             )}
         </>
     );
