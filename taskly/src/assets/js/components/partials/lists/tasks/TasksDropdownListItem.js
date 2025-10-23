2efa2430dc1a9479c66e7502c004ff2f04cf10a4
Balša Bazović <balsa.bazovic@gmail.com>
2025-10-23 11:18:10 -0400

    Updated Tasks view.

--- taskly/src/assets/js/components/partials/lists/tasks/TasksDropdownListItem.js
+++ taskly/src/assets/js/components/partials/lists/tasks/TasksDropdownListItem.js
@@ -4,7 +4,11 @@ import iconXCircleSrc from "../../../../../media/icons/x-circle.svg";
 
 export default function TaskDropdownListItem({ task }) {
     return (
-        <li className={`tasks-overview-tasks-list-item ${!task.resolved && "item-backtracked-task"}`}>
+        <li
+            className={`tasks-overview-tasks-list-item ${
+                !task.resolved && "backtracked-tasks-overview-tasks-list-item"
+            }`}
+        >
             <div className="div-tasks-overview-tasks-list-item-data-container">
                 <p>{task.title ?? "Title"}</p>
                 <span>Created On: {task.date ?? "Date"}</span>
